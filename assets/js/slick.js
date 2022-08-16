37f4c971febc324ff99711d17433b4ab33a8d264
Raket Biz Dev <rakethost@gmail.com>
2022-08-16 15:20:09 +0800

    add comment

--- assets/js/slick.js
+++ assets/js/slick.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
           }
         },
         {
-          breakpoint: 600,
+          breakpoint: 740,
           settings: {
             slidesToShow: 2,
             slidesToScroll: 2
@@ -26,6 +26,41 @@ $(document).ready(function () {
           breakpoint: 480,
           settings: {
             slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+$('.best-sellers').slick({
+      dots: false,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false
+          }
+        },
+        {
+          breakpoint: 740,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
             slidesToScroll: 1
           }
         }
